01555c9ab5015a8f50a13f0fe4420a8604384003
Shree Ganesh Yadav <yganesh692@gmail.com>
2025-03-16 13:59:31 +0530

    Added CartAPI Controller

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private ResponseDto _response;
+        private IMapper _mapper;
+        private readonly ApplicationDbContext _context;
+
+        public CartAPIController(ResponseDto response, IMapper mapper, ApplicationDbContext context)
+        {
+            _response = response;
+            _mapper = mapper;
+            _context = context;
+        }
+        [HttpPost("CartUpsert")]
+        public async Task<ResponseDto> CartUpsert(CartDto cartDto)
+        {
+
+        }
+    }
+}
