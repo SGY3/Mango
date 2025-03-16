86c5cc03d8883c6a3d6147ea9786ffd2ecec4857
Shree Ganesh Yadav <yganesh692@gmail.com>
2025-03-16 14:12:46 +0530

    Added Cart Logic

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -3,6 +3,7 @@ using Mango.Services.ShoppingCartAPI.Data;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.Controllers
 {
@@ -23,7 +24,35 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         [HttpPost("CartUpsert")]
         public async Task<ResponseDto> CartUpsert(CartDto cartDto)
         {
-
+            try
+            {
+                var cartHearderFromDb = await _context.CartHeaders.FirstOrDefaultAsync(u => u.UserId == cartDto.CartHeader.UserId);
+                if (cartHearderFromDb == null)
+                {
+                    //create header and details
+                }
+                else
+                {
+                    //if header is not null
+                    //check if details has some product
+                    var cartDetailsFromDb=await _context.CartDetails.FirstOrDefaultAsync(
+                        u=> u.ProductId==cartDto.CartDetails.First().ProductId &&
+                        u.CartHeaderId==cartHearderFromDb.CardHeaderId);
+                    if (cartDetailsFromDb == null)
+                    {
+                        //create cardDetails
+                    }
+                    else
+                    {
+                        //update count in cart details
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
         }
     }
 }
