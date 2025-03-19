286ee882952f8b0310b5821a096df15c3b4635a3
Shree Ganesh Yadav <yganesh692@gmail.com>
2025-03-19 20:34:23 +0530

    Added Cart Controller

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,36 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+        [Authorize]
+        public async Task<IActionResult> CartIndex()
+        {
+            return View(await LoadCartDtoBasedOnLoggedInUsaer());
+        }
+        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUsaer()
+        {
+            var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            ResponseDto? reponse = await _cartService.GetCartByUserIdAsync(userId);
+
+            if (reponse != null && reponse.IsSuccess)
+            {
+                CartDto cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(reponse.Result));
+                return cartDto;
+            }
+            return new CartDto();
+        }
+    }
+}
