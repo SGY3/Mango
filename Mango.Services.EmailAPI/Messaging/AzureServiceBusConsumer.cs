89a852127b888482e0f7360c6b7d428ea1a675db
Shree Ganesh Yadav <yganesh692@gmail.com>
2025-03-21 12:11:22 +0530

    Added Bus Service

---
+++ Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,64 @@
+﻿using Azure.Messaging.ServiceBus;
+using Mango.Services.EmailAPI.Models.Dto;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Mango.Services.EmailAPI.Messaging
+{
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
+    {
+        private readonly string serviceBusConnectionstring;
+        private readonly string emailCartQueue;
+        private readonly IConfiguration _configuration;
+
+        private ServiceBusProcessor _emailCartProcessor;
+
+        public AzureServiceBusConsumer(IConfiguration configuration)
+        {
+            serviceBusConnectionstring = _configuration.GetValue<string>("ServiceBusConnectionString");
+            emailCartQueue = _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue"); ;
+            _configuration = configuration;
+            var client = new ServiceBusClient(serviceBusConnectionstring);
+            _emailCartProcessor = client.CreateProcessor(emailCartQueue);
+        }
+
+        public async Task Start()
+        {
+            _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+            _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
+            await _emailCartProcessor.StartProcessingAsync();
+        }
+
+        private Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs args)
+        {
+            //this is where will receive message
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CartDto objMessage = JsonConvert.DeserializeObject<CartDto>(body);
+            try
+            {
+                //ToDo- try to  log email
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task Stop()
+        {
+            await _emailCartProcessor.StopProcessingAsync();
+            await _emailCartProcessor.DisposeAsync();
+        }
+    }
+}
